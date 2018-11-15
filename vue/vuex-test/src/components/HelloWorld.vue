3bde79e4583569c9843d23bfafe9769a06469272
wht <weihaitao01@chinatopcredit.com>
2018-11-15 19:49:46 +0800

    去掉动画

---
+++ vue/vuex-test/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+     <base-header></base-header>
+     <base-footer></base-footer>
+    </div>
+</template>
+
+<script>
+import BaseHeader from "../views/header"
+import BaseFooter from "../views/footer"
+export default{
+        props:["placeholder"],
+        components: { BaseHeader,BaseFooter },
+        data:function () {
+            return {
+                username:""
+            }
+        },
+        methods:{
+            userNameChange(){
+                this.$store.state.user_name = this.username;
+            }
+        }
+    }
+</script>
---
+++ vue/vuex-test/src/views/footer.vue
@@ -0,0 +1,31 @@
+<template>  
+  <footer class="footer"> 
+    <ul>   
+      <li v-for="lis in ul">{{lis.li}}</li>  
+    </ul>  
+    <p>Copyright © {{authorVal}} - 2016 All rights reserved  </p>
+  </footer> 
+</template>   
+
+<script>  
+export default {  
+  name: 'footerDiv',  
+  data () {   
+    return {   
+      ul: [
+         { li: '琉璃之金' },    
+         { li: '朦胧之森' },    
+         { li: '缥缈之滔' },    
+         { li: '逍遥之火' },    
+         { li: '璀璨之沙' }   
+      ]   
+    }  
+  },  
+
+  computed: {   
+    authorVal () {   
+      return this.$store.state.author   
+    }  
+  }  
+}
+</script>
